7b33aed7573103be8f039eb6c0b1d88f87c1c347
Robby-Blue <69482496+Robby-Blue@users.noreply.github.com>
2025-05-25 16:08:02 +0200

    ui: add lock rotation pref

--- app/src/main/java/me/robbyblue/mylauncher/LauncherApp.java
+++ app/src/main/java/me/robbyblue/mylauncher/LauncherApp.java
@@ -1,8 +1,15 @@
 package me.robbyblue.mylauncher;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.preference.PreferenceManager;
 
 public class LauncherApp extends Application {
     @Override
@@ -22,5 +29,58 @@ public class LauncherApp extends Application {
             android.os.Process.killProcess(android.os.Process.myPid());
             System.exit(1);
         });
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            @Override
+            public void onActivityCreated(Activity activity,
+                                          Bundle savedInstanceState) {
+                setRotation(activity);
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+                setRotation(activity);
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+
+            }
+
+        });
+    }
+
+    private void setRotation(Activity activity) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity.getBaseContext());
+        boolean allowRotations = prefs.getBoolean("pref_allow_rotation", false);
+
+        if (allowRotations) {
+            activity.setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+        } else {
+            activity.setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+        }
     }
 }
